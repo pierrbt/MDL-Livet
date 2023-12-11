15b875f97542ef0622a6b88dd56c536c9be1205b
pierrbt <pierre08bt@gmx.co.uk>
2023-12-11 22:35:27 +0100

    fix: api path

--- js/inscription.js
+++ js/inscription.js
@@ -37,7 +37,7 @@ form.addEventListener("submit", async (event) => {
 
     // On envoie l'utilisateur au serveur
     info.textContent = "Ajout de l'utilisateur ...";
-    const res = await fetch("https://api.mdl.veagle.fr/members", {
+    const res = await fetch("/api/members", {
       method: "POST",
       body: JSON.stringify(userObject),
       headers: {
