8b2f043afa07c530386ea4be7facc2c15dc24197
pierrbt <pierre08bt@gmx.co.uk>
2023-12-30 14:11:45 +0100

    refactor: format

--- js/clubs.js
+++ js/clubs.js
@@ -3,53 +3,53 @@
  * @type {{image: string, description: string, nom: string}[]}
  */
 const clubs = [
-    {
-        nom: "Club Argentique",
-        image: "img/clubs/argentique.svg",
-        description: "Cuisine, vente",
-    },
-    {
-        nom: "Club Numérique",
-        image: "img/clubs/numérique.svg",
-        description: "Arcade, rétro-découvertes, tournois",
-    },
-    {
-        nom: "Club plante",
-        image: "img/clubs/plante.svg",
-        description: "Super descriptio",
-    },
-    {
-        nom: "Club cuisine",
-        image: "img/clubs/cuisine.svg",
-        description: "Super descriptio",
-    },
-    {
-        nom: "Club gaming",
-        image: "img/clubs/gaming.svg",
-        description: "Super descriptio",
-    },
-    {
-        nom: "Club déco",
-        image: "img/clubs/déco.svg",
-        description: "Super descriptio",
-    },
-    {
-        nom: "Club graph",
-        image: "img/clubs/graph.svg",
-        description: "Super descriptio",
-    },
-    {
-        nom: "Club musique",
-        image: "img/clubs/musique.svg",
-        description: "Super descriptio",
-    },
+  {
+    nom: "Club Argentique",
+    image: "img/clubs/argentique.svg",
+    description: "Cuisine, vente",
+  },
+  {
+    nom: "Club Numérique",
+    image: "img/clubs/numérique.svg",
+    description: "Arcade, rétro-découvertes, tournois",
+  },
+  {
+    nom: "Club plante",
+    image: "img/clubs/plante.svg",
+    description: "Super descriptio",
+  },
+  {
+    nom: "Club cuisine",
+    image: "img/clubs/cuisine.svg",
+    description: "Super descriptio",
+  },
+  {
+    nom: "Club gaming",
+    image: "img/clubs/gaming.svg",
+    description: "Super descriptio",
+  },
+  {
+    nom: "Club déco",
+    image: "img/clubs/déco.svg",
+    description: "Super descriptio",
+  },
+  {
+    nom: "Club graph",
+    image: "img/clubs/graph.svg",
+    description: "Super descriptio",
+  },
+  {
+    nom: "Club musique",
+    image: "img/clubs/musique.svg",
+    description: "Super descriptio",
+  },
 ];
 
 const ul = document.querySelector("ul");
 
 ul.innerHTML = clubs
-    .map((club) => {
-        return `
+  .map((club) => {
+    return `
   <li>
     <img src="${club.image}" />
     <div>
@@ -58,5 +58,5 @@ ul.innerHTML = clubs
     </div>
   </li>
   `;
-    })
-    .join("");
+  })
+  .join("");
--- js/inscription.js
+++ js/inscription.js
@@ -1,37 +1,38 @@
 const tbody = document.querySelector("tbody");
 
 // On récupère les utilisateurs
-async function fetchUsers() { // On utilise une fonction asynchrone pour pouvoir utiliser await
-    try {
-        // On récupère les utilisateurs depuis le serveur
-        const req = await fetch("https://api.mdl.veagle.fr/members");
-        // On vérifie que la requête s'est bien passée
-        if (req.status !== 200)
-            throw new Error(
-                "Erreur lors de la récupération des membres sur le serveur",
-            );
-        // On récupère le JSON
-        const res = await req.json();
-        const users = res.data;
+async function fetchUsers() {
+  // On utilise une fonction asynchrone pour pouvoir utiliser await
+  try {
+    // On récupère les utilisateurs depuis le serveur
+    const req = await fetch("https://api.mdl.veagle.fr/members");
+    // On vérifie que la requête s'est bien passée
+    if (req.status !== 200)
+      throw new Error(
+        "Erreur lors de la récupération des membres sur le serveur",
+      );
+    // On récupère le JSON
+    const res = await req.json();
+    const users = res.data;
 
-        // On map chaque utilisateur pour créer une ligne du tableau
-        tbody.innerHTML = users
-            .map((user) => {
-                return `
+    // On map chaque utilisateur pour créer une ligne du tableau
+    tbody.innerHTML = users
+      .map((user) => {
+        return `
             <tr>
                 <td>${user.firstName}</td>
                 <td>${user.lastName}</td>
                 <td>${user.class}</td>
                 <td>${user.role}</td>
             </tr>`;
-            })
-            .join(""); // On join le tout pour éviter les virgules
-    } catch (err) {
-        console.error(err);
-        // On affiche une erreur dans le tableau si on a une erreur
-        tbody.innerHTML =
-            '<tr><td colspan="5">Erreur lors du chargement des utilisateurs</td></tr>';
-    }
+      })
+      .join(""); // On join le tout pour éviter les virgules
+  } catch (err) {
+    console.error(err);
+    // On affiche une erreur dans le tableau si on a une erreur
+    tbody.innerHTML =
+      '<tr><td colspan="5">Erreur lors du chargement des utilisateurs</td></tr>';
+  }
 }
 
 fetchUsers();
@@ -41,64 +42,64 @@ const form = document.querySelector("form");
 
 // On gère le onsubmit
 form.addEventListener("submit", async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    // On récupère les élements irrécupérables depuis event
-    const info = document.querySelector("#info");
-    const submit = document.querySelector(".submit");
+  // On récupère les élements irrécupérables depuis event
+  const info = document.querySelector("#info");
+  const submit = document.querySelector(".submit");
 
-    // On désactive le bouton submit pour le moment
-    submit.disabled = true;
+  // On désactive le bouton submit pour le moment
+  submit.disabled = true;
 
-    // On définit toutes les entrées du formulaire
-    const inputs = ["firstName", "lastName", "class", "age"];
+  // On définit toutes les entrées du formulaire
+  const inputs = ["firstName", "lastName", "class", "age"];
 
-    // On créé l'objet utilisateur par défaut
-    const userObject = {role: "Membre"};
+  // On créé l'objet utilisateur par défaut
+  const userObject = { role: "Membre" };
 
-    for (const input of inputs) {
-        // On vérifie la validité de l'entrée avec les paramètres dans l'HTML
-        const element = event.currentTarget[input];
-        if (!element.checkValidity()) {
-            info.textContent = "Une des entrées est invalide !";
-            return false;
-        }
-
-        // On récupère la valeur
-        const value = element.value;
-        userObject[input] = value;
-    }
-    // On commence le try pour récupérer toutes les exceptions de conversion et de requête HTTP
-    try {
-        // Conversion en Number pour l'age
-        userObject.age = Number(userObject.age);
-
-        // On envoie l'utilisateur au serveur
-        info.textContent = "Ajout de l'utilisateur ...";
-        const res = await fetch("https://api.mdl.veagle.fr/members", {
-            method: "POST",
-            body: JSON.stringify(userObject),
-            headers: {
-                // Important pour Fastify
-                "Content-Type": "application/json",
-            },
-        });
-
-        const body = await res.json();
-
-        // On gère une éventuelle erreur du serveur
-        if (res.status !== 200) {
-            throw new Error("Impossible de créer l'utilisateur");
-        }
-
-        info.textContent = "Utilisateur créé avec succès";
-    } catch (err) {
-        console.error(err);
-        info.textContent = "Erreur lors de la création de l'utilisateur";
-        // On réactive le bouton pour permettre à l'utilisateur de réessayer
-        submit.disabled = false;
-        return;
+  for (const input of inputs) {
+    // On vérifie la validité de l'entrée avec les paramètres dans l'HTML
+    const element = event.currentTarget[input];
+    if (!element.checkValidity()) {
+      info.textContent = "Une des entrées est invalide !";
+      return false;
     }
 
-    return true;
+    // On récupère la valeur
+    const value = element.value;
+    userObject[input] = value;
+  }
+  // On commence le try pour récupérer toutes les exceptions de conversion et de requête HTTP
+  try {
+    // Conversion en Number pour l'age
+    userObject.age = Number(userObject.age);
+
+    // On envoie l'utilisateur au serveur
+    info.textContent = "Ajout de l'utilisateur ...";
+    const res = await fetch("https://api.mdl.veagle.fr/members", {
+      method: "POST",
+      body: JSON.stringify(userObject),
+      headers: {
+        // Important pour Fastify
+        "Content-Type": "application/json",
+      },
+    });
+
+    const body = await res.json();
+
+    // On gère une éventuelle erreur du serveur
+    if (res.status !== 200) {
+      throw new Error("Impossible de créer l'utilisateur");
+    }
+
+    info.textContent = "Utilisateur créé avec succès";
+  } catch (err) {
+    console.error(err);
+    info.textContent = "Erreur lors de la création de l'utilisateur";
+    // On réactive le bouton pour permettre à l'utilisateur de réessayer
+    submit.disabled = false;
+    return;
+  }
+
+  return true;
 });
