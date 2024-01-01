0cd27cd2587ad12251eced612ad162b41fe4891f
pierrbt <pierre08bt@gmx.co.uk>
2024-01-01 23:48:31 +0100

    refactor: format

--- js/clubs.js
+++ js/clubs.js
@@ -3,46 +3,46 @@
  * @type {{image: string, description: string, nom: string}[]}
  */
 const clubs = [
-    {
-        nom: "Club Argentique",
-        image: "img/clubs/argentique.svg",
-        description: "Cuisine, vente",
-    },
-    {
-        nom: "Club Numérique",
-        image: "img/clubs/numérique.svg",
-        description: "Arcade, rétro-découvertes, tournois",
-    },
-    {
-        nom: "Club plante",
-        image: "img/clubs/plante.svg",
-        description: "Super descriptio",
-    },
-    {
-        nom: "Club cuisine",
-        image: "img/clubs/cuisine.svg",
-        description: "Super descriptio",
-    },
-    {
-        nom: "Club gaming",
-        image: "img/clubs/gaming.svg",
-        description: "Super descriptio",
-    },
-    {
-        nom: "Club déco",
-        image: "img/clubs/déco.svg",
-        description: "Super descriptio",
-    },
-    {
-        nom: "Club graph",
-        image: "img/clubs/graph.svg",
-        description: "Super descriptio",
-    },
-    {
-        nom: "Club musique",
-        image: "img/clubs/musique.svg",
-        description: "Super descriptio",
-    },
+  {
+    nom: "Club Argentique",
+    image: "img/clubs/argentique.svg",
+    description: "Cuisine, vente",
+  },
+  {
+    nom: "Club Numérique",
+    image: "img/clubs/numérique.svg",
+    description: "Arcade, rétro-découvertes, tournois",
+  },
+  {
+    nom: "Club plante",
+    image: "img/clubs/plante.svg",
+    description: "Super descriptio",
+  },
+  {
+    nom: "Club cuisine",
+    image: "img/clubs/cuisine.svg",
+    description: "Super descriptio",
+  },
+  {
+    nom: "Club gaming",
+    image: "img/clubs/gaming.svg",
+    description: "Super descriptio",
+  },
+  {
+    nom: "Club déco",
+    image: "img/clubs/déco.svg",
+    description: "Super descriptio",
+  },
+  {
+    nom: "Club graph",
+    image: "img/clubs/graph.svg",
+    description: "Super descriptio",
+  },
+  {
+    nom: "Club musique",
+    image: "img/clubs/musique.svg",
+    description: "Super descriptio",
+  },
 ];
 
 // On sélectionne l'élément <ul> dans lequel on va injecter les <li>
@@ -50,8 +50,8 @@ const ul = document.querySelector("ul");
 
 // On injecte les <li> dans le <ul> avec la méthode map()
 ul.innerHTML = clubs
-    .map((club) => {
-        return `
+  .map((club) => {
+    return `
   <li>
     <img src="${club.image}" />
     <div>
@@ -60,5 +60,5 @@ ul.innerHTML = clubs
     </div>
   </li>
   `;
-    })
-    .join(""); // On utilise join("") pour éviter les virgules entre les <li>
+  })
+  .join(""); // On utilise join("") pour éviter les virgules entre les <li>
